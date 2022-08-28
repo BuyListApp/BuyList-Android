093840b115df7ea932ee6b1a6fc7e82c9adeac26
Bimlibik <554476744j@gmail.com>
2022-08-28 21:09:03 +0800

    Добавлена сортировка рецептов.

--- app/src/main/java/ru/buylist/utils/PrefsUtil.kt
+++ app/src/main/java/ru/buylist/utils/PrefsUtil.kt
@@ -2,15 +2,29 @@ package ru.buylist.utils
 
 import android.content.Context
 import ru.buylist.BuyListApp
+import ru.buylist.presentation.data.RecipeSortKind
 
 private val prefs by lazy {
     BuyListApp.get().getSharedPreferences("BuyListPrefs", Context.MODE_PRIVATE)
 }
 
 private const val FIRST_START = "first start"
+private const val RECIPES_SORT_KIND = "recipes_sort_kind"
 
 fun getFirstStart(): Boolean = prefs.getBoolean(FIRST_START, true)
 
 fun changeFirstStart(isFirst: Boolean = false) {
     prefs.edit().putBoolean(FIRST_START, isFirst).apply()
+}
+
+fun getRecipesSortKind(): RecipeSortKind = prefs
+    .getString(RECIPES_SORT_KIND, RecipeSortKind.DATE_OF_CREATION.name)
+    ?.let { RecipeSortKind.valueOf(it) }
+    ?: RecipeSortKind.DATE_OF_CREATION
+
+fun saveRecipesSortKind(kind: RecipeSortKind) {
+    prefs
+        .edit()
+        .putString(RECIPES_SORT_KIND, kind.name)
+        .apply()
 }
